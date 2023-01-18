f70dc778767d15f000e9d405836389dc25359ab2
MelianMiko <melianmiko@yandex.ru>
2023-01-19 00:50:08 +0700

    No, that's too buggy, use hmApp.gotoPage

--- toolbox/utils/misc.js
+++ toolbox/utils/misc.js
@@ -3,10 +3,13 @@ const APP_ID = hmApp.packageInfo().appId;
 export function openPage(name, param=null) {
 	// Switching pages with this way reduces count of
 	// yellow screen appears
-	
-	hmApp.startApp({
-		appid: APP_ID,
-		url: "page/" + name,
-		param
-	})
+
+	// hmApp.startApp({
+	// 	appid: APP_ID,
+	// 	url: "page/" + name,
+	// 	param
+	// })
+
+	// No, that's too buggy
+	hmApp.gotoPage({url: "page/" + name, param});
 }
