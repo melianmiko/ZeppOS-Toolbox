4ca238c42232b944ec474eeec7bf4505cf71658c
MelianMiko <melianmiko@yandex.ru>
2023-01-18 22:29:46 +0700

    Redesign language picker

--- toolbox/page/SettingsLangScreen.js
+++ toolbox/page/SettingsLangScreen.js
@@ -1,40 +1,34 @@
-class SettingsLangScreen {
-	start() {
-		let current = hmFS.SysProGetChars("mmk_tb_lang");
-		if(!current) current = "false";
+import {SettingsListScreen} from "../../lib/SettingsListScreen";
 
-		const data = {
-			"false": "(system)",
-      "en-US": "English",
-      "zh-CN": "简体中文",
-      "zh-TW": "繁體中文",
-      "ru-RU": "Русский",
-		  "de-DE": "Deutsch"
-		};
+const available_locales = {
+  "en-US": "English",
+  "es-Es": "Español",
+  "de-DE": "Deutsch",
+  "ru-RU": "Русский",
+  "zh-CN": "简体中文",
+  "zh-TW": "繁體中文",
+};
 
-		let y = 64;
-		for(let key in data) {
-			this.makeRow(key, data[key], y, current === key);
-			y += 76;
-		}
+class SettingsLangScreen extends SettingsListScreen {
+	build() {
+		this.current = hmFS.SysProGetChars("mmk_tb_lang");
+		if(!this.current) this.current = "false";
+
+		const osLocale = DeviceRuntimeCore.HmUtils.getLanguage();
+
+		this.localeRow(`System (${osLocale})`, 'false');
+		this.headline("Supported:");
+
+		for(let key in available_locales)
+			this.localeRow(available_locales[key], key);
 	}
 
-	makeRow(key, name, y, active) {
-		hmUI.createWidget(hmUI.widget.BUTTON, {
-			x: 8,
-			y,
-			w: 192-16,
-			h: 64,
-			press_color: active ? 0x09253b : 0x333333,
-			normal_color: active ? 0x09253b : 0x222222,
-			radius: 12,
-			text: name,
-			color: active ? 0x008fff : 0xffffff,
-			click_func: () => {
-				hmFS.SysProSetChars("mmk_tb_lang", key);
-				hmApp.goBack();
-			}
-		})
+	localeRow(prettyName, key) {
+		const active = this.current == key;
+		this.clickableItem(prettyName, `menu/cb_${active}.png`, () => {
+			hmFS.SysProSetChars("mmk_tb_lang", key);
+			hmApp.goBack();
+		});
 	}
 }
 
